6795422e860053f3c7116d9ba5c1ce8547c5c615
Buicongbang04 <buicongbang1010@gmail.com>
2024-09-28 08:06:39 +0700

    draft

--- test_gpt/app.py
+++ test_gpt/app.py
@@ -13,7 +13,7 @@ def get_response(prompt):
         model='gpt-4-turbo',
         messages=[
             {'role': 'user', 'content': prompt},
-        ]
+        ],
     )
     return response.choices[0].message.content
 
